b0f13804c8e68be022a76552afa40d7e5d267814
Michael Fuget <michaelfuget@gmail.com>
2021-01-31 03:40:33 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     resources :posts, only: %i[show index new edit]
   end
 
+  scope '/admin' do
+  resources :stats, only: [:index]
+end
+
+
   resources :posts, only: %i[index show new create edit update]
 
   get '/admin/stats', to: 'stats#index'
